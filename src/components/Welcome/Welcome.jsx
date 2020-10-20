3830a10c6022ac9b484457b6ab4d6fe4015f36f5
Joe Pena <joempena26@gmail.com>
2020-10-20 11:00:27 -0600

    Welcome message styling

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -4,17 +4,19 @@ import { Card } from 'react-bootstrap';
 import './Welcome.scss';
 
 const Welcome = () => (
-  <Card className="bg-primary my-5 mx-3">
-    <Card.Body className="text-white text-left">
-      <Card.Title className="font-weight-bolder welcome-heading">
-        Finding Repositories has never been easier.
-      </Card.Title>
-      <Card.Text className="font-weight-light welcome-text">
-        You need to find a repo with the most stars, or the highest amount of
-        issues? That's why we're here.
-      </Card.Text>
-    </Card.Body>
-  </Card>
+  <div className="d-flex justify-content-center">
+    <Card className="bg-primary my-5 mx-3 col-lg-8">
+      <Card.Body className="text-white text-left">
+        <Card.Title className="font-weight-bolder welcome-heading">
+          Finding Repositories has never been easier.
+        </Card.Title>
+        <Card.Text className="font-weight-light welcome-text">
+          You need to find a repo with the most stars, or the highest amount of
+          issues? That's why we're here.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  </div>
 );
 
 export default Welcome;
